51a61dd96c94edee4d18c45217b8b3ddb3613722
Joey <jfrumento15@yahoo.com>
2025-11-05 16:10:40 -0800

    set 1 complete

--- hw9-jf.js
+++ hw9-jf.js
@@ -1,5 +1,26 @@
 var complexArithmeticResults = 3 + 5 * (10 / 2) - (8 - 4);
-console.log("E1:" + complexArithmeticResults);
+console.log("E1: " + complexArithmeticResults);
 
 //Exercise 2
-var projectIdentifier
+var projectIdentifier = "COMP484"
+ProjectIdentifier = "JS_Advanced"
+//This fails since the variable is not the correct spelling (P is capitalized)
+projectIdentifier = "JS_Advanced"
+console.log("E2: " + projectIdentifier);
+
+//Exercise 3
+var courseDescription = "The course is 'interactive' and involves 'scripting' logic."
+console.log("E3: " + courseDescription);
+
+//Exercise 4
+var errorMessage = 'An internal server error occurred: \"Access Denied\"'
+console.log("E4: " + errorMessage);
+
+//Exercise 5
+var valueA = 10
+var valueB = "4"
+var resultSubtraction = valueA - valueB
+var resultMulitiplication = valueA * valueB
+console.log("E5: " + valueA + " and " + valueB)
+console.log(typeof valueA)
+console.log(typeof valueB);
