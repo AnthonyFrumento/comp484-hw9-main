62ee39cfaed34460dc3b10d1b6130b588133db92
Joey <jfrumento15@yahoo.com>
2025-11-05 16:24:45 -0800

    Excercise 6 + reformatted output

--- hw9-jf.js
+++ hw9-jf.js
@@ -1,26 +1,45 @@
 var complexArithmeticResults = 3 + 5 * (10 / 2) - (8 - 4);
-console.log("E1: " + complexArithmeticResults);
+console.log("Exercise 1:")
+console.log(complexArithmeticResults);
 
 //Exercise 2
 var projectIdentifier = "COMP484"
 ProjectIdentifier = "JS_Advanced"
 //This fails since the variable is not the correct spelling (P is capitalized)
 projectIdentifier = "JS_Advanced"
-console.log("E2: " + projectIdentifier);
+console.log("")
+console.log("Exercise 2:")
+console.log(projectIdentifier);
 
 //Exercise 3
 var courseDescription = "The course is 'interactive' and involves 'scripting' logic."
-console.log("E3: " + courseDescription);
+console.log("")
+console.log("Exercise 3:")
+console.log(courseDescription);
 
 //Exercise 4
 var errorMessage = 'An internal server error occurred: \"Access Denied\"'
-console.log("E4: " + errorMessage);
+console.log("")
+console.log("Exercise 4:")
+console.log(errorMessage);
 
 //Exercise 5
 var valueA = 10
 var valueB = "4"
 var resultSubtraction = valueA - valueB
 var resultMulitiplication = valueA * valueB
-console.log("E5: " + valueA + " and " + valueB)
+console.log("")
+console.log("Exercise 5:")
+console.log(valueA)
+console.log(valueB)
 console.log(typeof valueA)
 console.log(typeof valueB);
+
+//Exercise 6
+var unassignedVar = ''
+var explicitNull = null
+console.log("")
+console.log("Exercise 6:")
+console.log(typeof unassignedVar)
+console.log(typeof explicitNull);
+//A blank string is still a string while null is an object
