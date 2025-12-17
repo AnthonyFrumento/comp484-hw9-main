d2e805edde6dabf84bd191ce004858add2f3856b
Joey <jfrumento15@yahoo.com>
2025-12-17 11:04:37 -0800

    fixed exercises 1-10

--- hw9-jf.js
+++ hw9-jf.js
@@ -1,86 +1,87 @@
+//Exercise 1
 var complexArithmeticResults = 3 + 5 * (10 / 2) - (8 - 4);
-console.log("Exercise 1:")
+console.log("Exercise 1:");
 console.log(complexArithmeticResults);
 
 //Exercise 2
 var projectIdentifier = "COMP484"
 ProjectIdentifier = "JS_Advanced"
-//This fails since the variable is not the correct spelling (P is capitalized)
+//This fails because JavaScript is case-sensitive. "ProjectIdentifier" is not the same variable as "projectIdentifier"
 projectIdentifier = "JS_Advanced"
-console.log("")
-console.log("Exercise 2:")
+console.log("");
+console.log("Exercise 2:");
 console.log(projectIdentifier);
 
 //Exercise 3
 var courseDescription = "The course is 'interactive' and involves 'scripting' logic."
-console.log("")
-console.log("Exercise 3:")
+console.log("");
+console.log("Exercise 3:");
 console.log(courseDescription);
 
 //Exercise 4
 var errorMessage = 'An internal server error occurred: \"Access Denied\"'
-console.log("")
-console.log("Exercise 4:")
+console.log("");
+console.log("Exercise 4:");
 console.log(errorMessage);
 
 //Exercise 5
-var valueA = 10
-var valueB = "4"
-var resultSubtraction = valueA - valueB
-var resultMulitiplication = valueA * valueB
-console.log("")
-console.log("Exercise 5:")
-console.log(valueA)
-console.log(valueB)
-console.log(typeof valueA)
-console.log(typeof valueB);
+var valueA = 10;
+var valueB = "4";
+var resultSubtraction = valueA - valueB;
+var resultMultiplication = valueA * valueB;
+
+console.log("\nExercise 5:");
+console.log("Subtraction:", resultSubtraction);
+console.log("Multiplication:", resultMultiplication);
+console.log("typeof valueA:", typeof valueA);
+console.log("typeof valueB:", typeof valueB);
 
 //Exercise 6
-var unassignedVar = ''
-var explicitNull = null
-console.log("")
-console.log("Exercise 6:")
-console.log(typeof unassignedVar)
+var unassignedVar;
+var explicitNull = null;
+console.log("");
+console.log("Exercise 6:");
+console.log(typeof unassignedVar);
 console.log(typeof explicitNull);
 //A blank string is still a string while null is an object
 
 //Exercise 7
-console.log("")
-console.log("Exercise 7:")
-var isBlocking = true
-console.log(typeof isBlocking)
-isBlocking = "true"
+console.log("");
+console.log("Exercise 7:");
+var isBlocking = true;
+console.log(typeof isBlocking);
+isBlocking = "true";
 console.log(typeof isBlocking);
 
 //Exercise 8
-console.log("")
-console.log("Exercise 8:")
+console.log("");
+console.log("Exercise 8:");
 //var 1test = test
 //cant start variable name with a number
 //var test 2 = test
 //cant add a space in variable name
 //var "test" = test
 //cant use special characters like "" in variable name
-console.log("Check Comments")
+console.log("Check Comments");
 
 //Exercise 9
-console.log("")
-console.log("Exercise 9:")
-var counterValue = 50
-counterValue = counterValue /= 5
-counterValue = counterValue -= 3
-console.log(counterValue)
+console.log("");
+console.log("Exercise 9:");
+var counterValue = 50;
+counterValue /= 5;
+counterValue -= 3;
+console.log(counterValue);
 
 //Exercise 10
-console.log("")
-console.log("Exercise 10:")
-var x = 10
-var y_post = x++
-console.log(y_post)
-console.log(x)
-x = 10
-var z_pre = ++x
-console.log(z_pre)
-console.log(x)
-//the two + after the x adds to the y_post
-//the two + before the x reinitializes x and assigns to z_pre
+console.log("");
+console.log("Exercise 10:");
+var x = 10;
+var y_post = x++;
+console.log(y_post);
+console.log(x);
+x = 10;
+var z_pre = ++x;
+console.log(z_pre);
+console.log(x);
+// x++ returns the value before incrementing
+// ++x increments before returning the value
